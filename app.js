d3c44d1eb3abaebd29cba667e5b4125f5dcd5c3e
Nori <nori.terada9@gmail.com>
2022-06-21 10:46:50 -0600

    added styles to the page andworking buttons

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 
 const container = document.querySelector(".container")
+const clear = document.querySelector("#clear")
+const black = document.querySelector("#black")
+const random = document.querySelector("#random")
+const eraser = document.querySelector("#eraser")
 
-
-for (i = 0; i < 16; i++){  
+for (i = 0; i < 144; i++){  
     const div = document.createElement("div")
     container.append(div);
     div.classList.add('items');
@@ -16,5 +19,34 @@ document.querySelectorAll(".manyDivs").forEach(item =>{
 })
 
 
+clear.addEventListener("click", function clearBoard(){
+    document.querySelectorAll(".manyDivs").forEach(item =>{
+        item.style.backgroundColor = "white";
+    })
+})
+
+black.addEventListener("click", function toBlack(){
+    document.querySelectorAll(".manyDivs").forEach(item =>{
+        item.addEventListener("mouseover", function(){
+            item.style.backgroundColor = "black";
+        })
+    })
+})
+random.addEventListener("click", function toRandom(){
+    document.querySelectorAll(".manyDivs").forEach(item =>{
+        item.addEventListener("mouseover", function(){
+            let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+            item.style.backgroundColor = randomColor;
+        })
+    })
+})
+
+eraser.addEventListener("click", function toWhite(){
+    document.querySelectorAll(".manyDivs").forEach(item =>{
+        item.addEventListener("mouseover", function(){
+            item.style.backgroundColor = "white";
+        })
+    })
+})
 
 
